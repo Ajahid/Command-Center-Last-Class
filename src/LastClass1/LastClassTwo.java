24031923f2fba1a21ce970c8c58c07c274abe7a7
Ajahid <jahidabdulmajeed@gmail.com>
2022-01-09 20:02:30 -0800

    Class Two Code Ajahid

---
+++ src/LastClass1/LastClassTwo.java
@@ -0,0 +1,12 @@
+package LastClass1;
+
+public class LastClassTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Practice using Command Center");
+		
+	}
+
+}
